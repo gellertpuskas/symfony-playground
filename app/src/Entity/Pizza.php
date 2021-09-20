74e61cfea0e98af6857ea641ca7a91202854581b
Puskás Gellért <devgellert@gmail.com>
2021-09-20 12:36:28 +0200

    Pizza entity's xml mapping

--- app/src/Entity/Pizza.php
+++ app/src/Entity/Pizza.php
@@ -2,24 +2,10 @@
 
 namespace App\Entity;
 
-use App\Repository\PizzaRepository;
-use Doctrine\ORM\Mapping as ORM;
-
-/**
- * @ORM\Entity(repositoryClass=PizzaRepository::class)
- */
 class Pizza
 {
-    /**
-     * @ORM\Id
-     * @ORM\GeneratedValue
-     * @ORM\Column(type="integer")
-     */
     private $id;
 
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
     private $name;
 
     public function getId(): ?int
