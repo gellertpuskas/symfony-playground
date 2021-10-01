9ee79fde61dad6fa7596afe84a7999c479bddb2f
Puskas Gellert <puskas.gellert@straxus.hu>
2021-10-01 16:11:02 +0200

    user cr;

---
+++ app/src/Controller/UserController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use App\Repository\UserRepository;
+use \Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+class UserController extends AbstractController {
+
+    /** @var UserRepository */
+    private $userRepository;
+
+    /** @var ValidatorInterface */
+    private $validator;
+
+    public function __construct(UserRepository $userRepository, ValidatorInterface $validator)
+    {
+        $this->userRepository = $userRepository;
+        $this->validator = $validator;
+    }
+
+
+    public function index() {
+        $users = $this->userRepository->findAll();
+        return $this->render("user/index.html.twig", [ "users" => $users ]);
+    }
+
+    public function create(Request $request) {
+        $user = new User();
+        $user->setEmail("example@gmail.com");
+
+        $userForm = $this->createForm(UserType::class, $user, [
+            "action" => $this->generateUrl("user_create"),
+            "method" => "POST"
+        ]);
+
+        $userForm->handleRequest($request);
+
+        if($userForm->isSubmitted()) {
+            $result = $this->store($userForm);
+
+            return $this->redirectToRoute("pizza_index"); // TODO success
+        }
+
+        return $this->renderForm("user/create.html.twig", [
+            "form" => $userForm
+        ]);
+    }
+
+    private function store(FormInterface $form) {
+        $errors = $this->validator->validate($form);
+        if(count($errors) !== 0)
+        {
+            return [
+                "success" => false,
+                "user" => null
+            ];
+        }
+
+        $em = $this->getDoctrine()->getManager();
+
+        $user = $form->getData();
+        $em->persist($user);
+        $em->flush();
+
+        return [
+            "success" => true,
+            "pizza" => $user
+        ];
+    }
+}
